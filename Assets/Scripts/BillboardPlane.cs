a843a1777e3be8096935448d66cb3a9106fce791
Dominik Gałenza <dominik.galenza@gmail.com>
2022-06-21 21:32:29 +0200

    Finished chapter 9

---
+++ Assets/Scripts/BillboardPlane.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardPlane : MonoBehaviour
+{
+    public Camera theCamera;
+
+    void Update()
+    {
+        transform.LookAt(2 * transform.position - theCamera.transform.position);
+    }
+}
